acbd79cc123d07b7f27ee0b9408854029f843b15
megu3k <49593780+megu3k@users.noreply.github.com>
2021-03-09 23:05:52 +0900

    update delete function

--- src/index.js
+++ src/index.js
@@ -16,15 +16,19 @@ const onClickAdd = () => {
   // button(完了)タグ
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
-  completeButton.addEventListener("click", () => {});
+  completeButton.addEventListener("click", () => {
+    // 押された完了ボタンの親タグ(div)を完了リストに移動
+
+    // 押された完了ボタンの親タグ(div)を完了リストから削除
+    deleteFromIncompleteList(completeButton.parentNode);
+  });
 
   // button(削除)タグ
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
     // 押された削除ボタンの親タグ(div)を未完了リストから削除
-    const deleteTaret = deleteButton.parentNode;
-    document.getElementById("incomplete-list").removeChild(deleteTaret);
+    deleteFromIncompleteList(deleteButton.parentNode);
   });
 
   // divタグの子要素に各要素を設定
@@ -34,6 +38,11 @@ const onClickAdd = () => {
 
   //未完了のリストに追加
   document.getElementById("incomplete-list").appendChild(div);
+};
+
+// 未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
 };
 
 document
